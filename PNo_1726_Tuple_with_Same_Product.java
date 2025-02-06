4eec6746f0cf9b8332fd3035d2faf7711fdfb5a7
Nikhil Chikate <nikhilchikate07@gmail.com>
2025-02-06 11:17:33 +0530

    added #1726 solns

---
+++ PNo_1726_Tuple_with_Same_Product.java
@@ -0,0 +1,44 @@
+/*
+Given an array nums of distinct positive integers, return the number of tuples (a, b, c, d) such that a * b = c * d where a, b, c, and d are elements of nums, and a != b != c != d.
+
+
+
+Example 1:
+
+Input: nums = [2,3,4,6]
+Output: 8
+Explanation: There are 8 valid tuples:
+(2,6,3,4) , (2,6,4,3) , (6,2,3,4) , (6,2,4,3)
+(3,4,2,6) , (4,3,2,6) , (3,4,6,2) , (4,3,6,2)
+Example 2:
+
+Input: nums = [1,2,4,5,10]
+Output: 16
+Explanation: There are 16 valid tuples:
+(1,10,2,5) , (1,10,5,2) , (10,1,2,5) , (10,1,5,2)
+(2,5,1,10) , (2,5,10,1) , (5,2,1,10) , (5,2,10,1)
+(2,10,4,5) , (2,10,5,4) , (10,2,4,5) , (10,2,5,4)
+(4,5,2,10) , (4,5,10,2) , (5,4,2,10) , (5,4,10,2)
+
+
+Constraints:
+
+1 <= nums.length <= 1000
+1 <= nums[i] <= 104
+All elements in nums are distinct.
+*/
+
+class Solution {
+    public int tupleSameProduct(int[] arr) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int cnt = 0;
+        for(int x = 0; x < arr.length; x++) {
+            for(int y = x + 1; y < arr.length; y++) {
+                int prod = arr[x] * arr[y];
+                cnt += (hm.getOrDefault(prod, 0) * 8);
+                hm.merge(prod, 1, Integer::sum);
+            }
+        }
+        return cnt;
+    }
+}
