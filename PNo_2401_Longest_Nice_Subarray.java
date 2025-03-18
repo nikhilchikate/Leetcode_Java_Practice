2488da0af963e1d0ea1fa56c0de97661c47a0911
Nikhil Chikate <nikhilchikate07@gmail.com>
2025-03-18 07:40:05 +0530

    added #2401 solns

---
+++ PNo_2401_Longest_Nice_Subarray.java
@@ -0,0 +1,53 @@
+/*
+You are given an array nums consisting of positive integers.
+
+We call a subarray of nums nice if the bitwise AND of every pair of elements that are in different positions in the subarray is equal to 0.
+
+Return the length of the longest nice subarray.
+
+A subarray is a contiguous part of an array.
+
+Note that subarrays of length 1 are always considered nice.
+
+
+
+Example 1:
+
+Input: nums = [1,3,8,48,10]
+Output: 3
+Explanation: The longest nice subarray is [3,8,48]. This subarray satisfies the conditions:
+- 3 AND 8 = 0.
+- 3 AND 48 = 0.
+- 8 AND 48 = 0.
+It can be proven that no longer nice subarray can be obtained, so we return 3.
+Example 2:
+
+Input: nums = [3,1,5,11,13]
+Output: 1
+Explanation: The length of the longest nice subarray is 1. Any subarray of length 1 can be chosen.
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 109
+*/
+
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int ans = 0;
+        int used = 0;
+        int l = 0;
+
+        for (int r = 0; r < nums.length; ++r) {
+            while ((used & nums[r]) != 0) {
+                used ^= nums[l];
+                l++;
+            }
+            used |= nums[r];
+            ans = Math.max(ans, r - l + 1);
+        }
+
+        return ans;
+    }
+}
