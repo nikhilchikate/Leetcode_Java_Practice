5cbaa7d943b6a60b7b808c9699e7a66f68f6d821
Nikhil Chikate <nikhilchikate07@gmail.com>
2025-04-04 08:49:16 +0530

    added #865, #1123 solns

---
+++ PNo_865_Smallest_Subtree_with_all_the_Deepest_Nodes.java
@@ -0,0 +1,85 @@
+/*
+Given the root of a binary tree, the depth of each node is the shortest distance to the root.
+
+Return the smallest subtree such that it contains all the deepest nodes in the original tree.
+
+A node is called the deepest if it has the largest depth possible among any node in the entire tree.
+
+The subtree of a node is a tree consisting of that node, plus the set of all descendants of that node.
+
+
+
+Example 1:
+
+
+Input: root = [3,5,1,6,2,0,8,null,null,7,4]
+Output: [2,7,4]
+Explanation: We return the node with value 2, colored in yellow in the diagram.
+The nodes coloured in blue are the deepest nodes of the tree.
+Notice that nodes 5, 3 and 2 contain the deepest nodes in the tree but node 2 is the smallest subtree among them, so we return it.
+Example 2:
+
+Input: root = [1]
+Output: [1]
+Explanation: The root is the deepest node in the tree.
+Example 3:
+
+Input: root = [0,1,3,null,2]
+Output: [2]
+Explanation: The deepest node in the tree is 2, the valid subtrees are the subtrees of nodes 2, 1 and 0 but the subtree of node 2 is the smallest.
+
+
+Constraints:
+
+The number of nodes in the tree will be in the range [1, 500].
+0 <= Node.val <= 500
+The values of the nodes in the tree are unique.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        return findLCA(root).node;
+    }
+
+    private static class Result {
+        TreeNode node;
+        int depth;
+
+        Result(TreeNode node, int depth) {
+            this.node = node;
+            this.depth = depth;
+        }
+    }
+
+    private Result findLCA(TreeNode curr) {
+        if (curr == null) {
+            return new Result(null, 0);
+        }
+
+        Result left = findLCA(curr.left);
+        Result right = findLCA(curr.right);
+
+        if (left.depth > right.depth) {
+            return new Result(left.node, left.depth + 1);
+        } else if (right.depth > left.depth) {
+            return new Result(right.node, right.depth + 1);
+        } else {
+            return new Result(curr, left.depth + 1);
+        }
+    }
+}
