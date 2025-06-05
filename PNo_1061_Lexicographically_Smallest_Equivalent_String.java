ba387c9acf0eac17e548c59020bc2398c99d3e5f
Nikhil Chikate <nikhilchikate07@gmail.com>
2025-06-05 21:40:06 +0530

    added #1061 solns

---
+++ PNo_1061_Lexicographically_Smallest_Equivalent_String.java
@@ -0,0 +1,84 @@
+/*
+You are given two strings of the same length s1 and s2 and a string baseStr.
+
+We say s1[i] and s2[i] are equivalent characters.
+
+For example, if s1 = "abc" and s2 = "cde", then we have 'a' == 'c', 'b' == 'd', and 'c' == 'e'.
+Equivalent characters follow the usual rules of any equivalence relation:
+
+Reflexivity: 'a' == 'a'.
+Symmetry: 'a' == 'b' implies 'b' == 'a'.
+Transitivity: 'a' == 'b' and 'b' == 'c' implies 'a' == 'c'.
+For example, given the equivalency information from s1 = "abc" and s2 = "cde", "acd" and "aab" are equivalent strings of baseStr = "eed", and "aab" is the lexicographically smallest equivalent string of baseStr.
+
+Return the lexicographically smallest equivalent string of baseStr by using the equivalency information from s1 and s2.
+
+
+
+Example 1:
+
+Input: s1 = "parker", s2 = "morris", baseStr = "parser"
+Output: "makkek"
+Explanation: Based on the equivalency information in s1 and s2, we can group their characters as [m,p], [a,o], [k,r,s], [e,i].
+The characters in each group are equivalent and sorted in lexicographical order.
+So the answer is "makkek".
+Example 2:
+
+Input: s1 = "hello", s2 = "world", baseStr = "hold"
+Output: "hdld"
+Explanation: Based on the equivalency information in s1 and s2, we can group their characters as [h,w], [d,e,o], [l,r].
+So only the second letter 'o' in baseStr is changed to 'd', the answer is "hdld".
+Example 3:
+
+Input: s1 = "leetcode", s2 = "programs", baseStr = "sourcecode"
+Output: "aauaaaaada"
+Explanation: We group the equivalent characters in s1 and s2 as [a,o,e,r,s,c], [l,p], [g,t] and [d,m], thus all letters in baseStr except 'u' and 'd' are transformed to 'a', the answer is "aauaaaaada".
+
+
+Constraints:
+
+1 <= s1.length, s2.length, baseStr <= 1000
+s1.length == s2.length
+s1, s2, and baseStr consist of lowercase English letters.
+*/
+
+class Solution {
+    static int[] groupRep;
+
+    public static int find(int val) {
+        if (groupRep[val] != val) {
+            groupRep[val] = find(groupRep[val]);
+        }
+        return groupRep[val];
+    }
+
+    public static void union(int valA, int valB) {
+        int rootA = find(valA);
+        int rootB = find(valB);
+
+        if (rootA < rootB) {
+            groupRep[rootB] = rootA;
+        } else {
+            groupRep[rootA] = rootB;
+        }
+    }
+
+    public String smallestEquivalentString(String s1, String s2, String baseStr) {
+        int strLen = baseStr.length();
+        groupRep = new int[26];
+        for (int i = 0; i < 26; i++) {
+            groupRep[i] = i;
+        }
+
+        for (int i = 0; i < s1.length(); i++) {
+            union(s1.charAt(i) - 'a', s2.charAt(i) - 'a');
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < strLen; i++) {
+            char currChar = baseStr.charAt(i);
+            result.append((char) (find(currChar - 'a') + 'a'));
+        }
+        return result.toString();
+    }
+}
