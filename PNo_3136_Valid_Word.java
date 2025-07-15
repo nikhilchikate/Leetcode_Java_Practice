44fb09a3cb20281e0b7f772cc15513cb728a93b7
Nikhil Chikate <nikhilchikate07@gmail.com>
2025-07-15 09:29:11 +0530

    added #3136 solns

---
+++ PNo_3136_Valid_Word.java
@@ -0,0 +1,84 @@
+/*
+A word is considered valid if:
+
+It contains a minimum of 3 characters.
+It contains only digits (0-9), and English letters (uppercase and lowercase).
+It includes at least one vowel.
+It includes at least one consonant.
+You are given a string word.
+
+Return true if word is valid, otherwise, return false.
+
+Notes:
+
+'a', 'e', 'i', 'o', 'u', and their uppercases are vowels.
+A consonant is an English letter that is not a vowel.
+
+
+Example 1:
+
+Input: word = "234Adas"
+
+Output: true
+
+Explanation:
+
+This word satisfies the conditions.
+
+Example 2:
+
+Input: word = "b3"
+
+Output: false
+
+Explanation:
+
+The length of this word is fewer than 3, and does not have a vowel.
+
+Example 3:
+
+Input: word = "a3$e"
+
+Output: false
+
+Explanation:
+
+This word contains a '$' character and does not have a consonant.
+
+
+
+Constraints:
+
+1 <= word.length <= 20
+word consists of English uppercase and lowercase letters, digits, '@', '#', and '$'.
+*/
+
+class Solution {
+    public boolean isValid(String word) {
+        if (word.length() < 3)
+            return false;
+
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+
+        for (int i = 0; i < word.length(); ++i) {
+            switch (word.charAt(i)) {
+                case 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U':
+                    hasVowel = true;
+                    break;
+                case 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y',
+                     'z',
+                     'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X',
+                     'Y', 'Z':
+                    hasConsonant = true;
+                    break;
+                case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+                    break;
+                default:
+                    return false;
+            }
+        }
+
+        return hasVowel && hasConsonant;
+    }
+}
