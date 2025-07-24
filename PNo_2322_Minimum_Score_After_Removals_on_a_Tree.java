e0005091fbe6e78c12d182cfe0bd59fb3bab1c99
Nikhil Chikate <nikhilchikate07@gmail.com>
2025-07-24 09:16:28 +0530

    added #2322 solns

---
+++ PNo_2322_Minimum_Score_After_Removals_on_a_Tree.java
@@ -0,0 +1,116 @@
+/*
+There is an undirected connected tree with n nodes labeled from 0 to n - 1 and n - 1 edges.
+
+You are given a 0-indexed integer array nums of length n where nums[i] represents the value of the ith node. You are also given a 2D integer array edges of length n - 1 where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the tree.
+
+Remove two distinct edges of the tree to form three connected components. For a pair of removed edges, the following steps are defined:
+
+Get the XOR of all the values of the nodes for each of the three components respectively.
+The difference between the largest XOR value and the smallest XOR value is the score of the pair.
+For example, say the three components have the node values: [4,5,7], [1,9], and [3,3,3]. The three XOR values are 4 ^ 5 ^ 7 = 6, 1 ^ 9 = 8, and 3 ^ 3 ^ 3 = 3. The largest XOR value is 8 and the smallest XOR value is 3. The score is then 8 - 3 = 5.
+Return the minimum score of any possible pair of edge removals on the given tree.
+
+
+
+Example 1:
+
+
+Input: nums = [1,5,5,4,11], edges = [[0,1],[1,2],[1,3],[3,4]]
+Output: 9
+Explanation: The diagram above shows a way to make a pair of removals.
+- The 1st component has nodes [1,3,4] with values [5,4,11]. Its XOR value is 5 ^ 4 ^ 11 = 10.
+- The 2nd component has node [0] with value [1]. Its XOR value is 1 = 1.
+- The 3rd component has node [2] with value [5]. Its XOR value is 5 = 5.
+The score is the difference between the largest and smallest XOR value which is 10 - 1 = 9.
+It can be shown that no other pair of removals will obtain a smaller score than 9.
+Example 2:
+
+
+Input: nums = [5,5,2,4,4,2], edges = [[0,1],[1,2],[5,2],[4,3],[1,3]]
+Output: 0
+Explanation: The diagram above shows a way to make a pair of removals.
+- The 1st component has nodes [3,4] with values [4,4]. Its XOR value is 4 ^ 4 = 0.
+- The 2nd component has nodes [1,0] with values [5,5]. Its XOR value is 5 ^ 5 = 0.
+- The 3rd component has nodes [2,5] with values [2,2]. Its XOR value is 2 ^ 2 = 0.
+The score is the difference between the largest and smallest XOR value which is 0 - 0 = 0.
+We cannot obtain a smaller score than 0.
+
+
+Constraints:
+
+n == nums.length
+3 <= n <= 1000
+1 <= nums[i] <= 108
+edges.length == n - 1
+edges[i].length == 2
+0 <= ai, bi < n
+ai != bi
+edges represents a valid tree.
+*/
+
+class Solution {
+
+    public int minimumScore(int[] values, int[][] connections) {
+        int numNodes = values.length;
+        List<List<Integer>> adjacency = new ArrayList<>();
+        for (int i = 0; i < numNodes; i++) {
+            adjacency.add(new ArrayList<>());
+        }
+        for (int[] conn : connections) {
+            adjacency.get(conn[0]).add(conn[1]);
+            adjacency.get(conn[1]).add(conn[0]);
+        }
+
+        int[] xorSum = new int[numNodes];
+        int[] entryTime = new int[numNodes];
+        int[] exitTime = new int[numNodes];
+        int[] timeCounter = { 0 };
+
+        depthFirstSearch(0, -1, values, adjacency, xorSum, entryTime, exitTime, timeCounter);
+        int minResult = Integer.MAX_VALUE;
+        for (int nodeU = 1; nodeU < numNodes; nodeU++) {
+            for (int nodeV = nodeU + 1; nodeV < numNodes; nodeV++) {
+                if (entryTime[nodeV] > entryTime[nodeU] && entryTime[nodeV] < exitTime[nodeU]) {
+                    minResult = Math.min(
+                            minResult,
+                            calculateDiff(xorSum[0] ^ xorSum[nodeU], xorSum[nodeU] ^ xorSum[nodeV], xorSum[nodeV]));
+                } else if (entryTime[nodeU] > entryTime[nodeV] && entryTime[nodeU] < exitTime[nodeV]) {
+                    minResult = Math.min(
+                            minResult,
+                            calculateDiff(xorSum[0] ^ xorSum[nodeV], xorSum[nodeV] ^ xorSum[nodeU], xorSum[nodeU]));
+                } else {
+                    minResult = Math.min(
+                            minResult,
+                            calculateDiff(xorSum[0] ^ xorSum[nodeU] ^ xorSum[nodeV], xorSum[nodeU], xorSum[nodeV]));
+                }
+            }
+        }
+        return minResult;
+    }
+
+    private int calculateDiff(int p1, int p2, int p3) {
+        return (Math.max(p1, Math.max(p2, p3)) -
+                Math.min(p1, Math.min(p2, p3)));
+    }
+
+    private void depthFirstSearch(
+            int current,
+            int parent,
+            int[] values,
+            List<List<Integer>> adjacency,
+            int[] xorSum,
+            int[] entryTime,
+            int[] exitTime,
+            int[] timeCounter) {
+        entryTime[current] = timeCounter[0]++;
+        xorSum[current] = values[current];
+        for (int neighbor : adjacency.get(current)) {
+            if (neighbor == parent) {
+                continue;
+            }
+            depthFirstSearch(neighbor, current, values, adjacency, xorSum, entryTime, exitTime, timeCounter);
+            xorSum[current] ^= xorSum[neighbor];
+        }
+        exitTime[current] = timeCounter[0];
+    }
+}
