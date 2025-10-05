0eeee04fe48cf471f6e836a1ffbebd0699db627c
Nikhil Chikate <nikhilchikate07@gmail.com>
2025-10-05 11:10:15 +0530

    added #417 solns

---
+++ PNo_417_Pacific_Atlantic_Water_Flow.java
@@ -0,0 +1,89 @@
+/*
+There is an m x n rectangular island that borders both the Pacific Ocean and Atlantic Ocean. The Pacific Ocean touches the island's left and top edges, and the Atlantic Ocean touches the island's right and bottom edges.
+
+The island is partitioned into a grid of square cells. You are given an m x n integer matrix heights where heights[r][c] represents the height above sea level of the cell at coordinate (r, c).
+
+The island receives a lot of rain, and the rain water can flow to neighboring cells directly north, south, east, and west if the neighboring cell's height is less than or equal to the current cell's height. Water can flow from any cell adjacent to an ocean into the ocean.
+
+Return a 2D list of grid coordinates result where result[i] = [ri, ci] denotes that rain water can flow from cell (ri, ci) to both the Pacific and Atlantic oceans.
+
+
+
+Example 1:
+
+
+Input: heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+Output: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
+Explanation: The following cells can flow to the Pacific and Atlantic oceans, as shown below:
+[0,4]: [0,4] -> Pacific Ocean
+       [0,4] -> Atlantic Ocean
+[1,3]: [1,3] -> [0,3] -> Pacific Ocean
+       [1,3] -> [1,4] -> Atlantic Ocean
+[1,4]: [1,4] -> [1,3] -> [0,3] -> Pacific Ocean
+       [1,4] -> Atlantic Ocean
+[2,2]: [2,2] -> [1,2] -> [0,2] -> Pacific Ocean
+       [2,2] -> [2,3] -> [2,4] -> Atlantic Ocean
+[3,0]: [3,0] -> Pacific Ocean
+       [3,0] -> [4,0] -> Atlantic Ocean
+[3,1]: [3,1] -> [3,0] -> Pacific Ocean
+       [3,1] -> [4,1] -> Atlantic Ocean
+[4,0]: [4,0] -> Pacific Ocean
+       [4,0] -> Atlantic Ocean
+Note that there are other possible paths for these cells to flow to the Pacific and Atlantic oceans.
+Example 2:
+
+Input: heights = [[1]]
+Output: [[0,0]]
+Explanation: The water can flow from the only cell to the Pacific and Atlantic oceans.
+
+
+Constraints:
+
+m == heights.length
+n == heights[r].length
+1 <= m, n <= 200
+0 <= heights[r][c] <= 105
+*/
+
+class Solution {
+    public List<List<Integer>> pacificAtlantic(int[][] h) {
+        final int R = h.length;
+        final int C = h[0].length;
+        List<List<Integer>> res = new ArrayList<>();
+        boolean[][] pSeen = new boolean[R][C];
+        boolean[][] aSeen = new boolean[R][C];
+
+        for (int r = 0; r < R; ++r) {
+            dfs(h, r, 0, 0, pSeen);
+            dfs(h, r, C - 1, 0, aSeen);
+        }
+
+        for (int c = 0; c < C; ++c) {
+            dfs(h, 0, c, 0, pSeen);
+            dfs(h, R - 1, c, 0, aSeen);
+        }
+
+        for (int r = 0; r < R; ++r) {
+            for (int c = 0; c < C; ++c) {
+                if (pSeen[r][c] && aSeen[r][c])
+                    res.add(new ArrayList<>(List.of(r, c)));
+            }
+        }
+        return res;
+    }
+
+    private void dfs(final int[][] h, int r, int c, int prevH, boolean[][] seen) {
+        if (r < 0 || r == h.length || c < 0 || c == h[0].length)
+            return;
+        if (seen[r][c] || h[r][c] < prevH)
+            return;
+
+        seen[r][c] = true;
+        int currentH = h[r][c];
+
+        dfs(h, r + 1, c, currentH, seen);
+        dfs(h, r - 1, c, currentH, seen);
+        dfs(h, r, c + 1, currentH, seen);
+        dfs(h, r, c - 1, currentH, seen);
+    }
+}
