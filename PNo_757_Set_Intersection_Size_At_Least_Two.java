09af30b619eac0d5c2c29634874b007eb429c867
Nikhil Chikate <nikhilchikate07@gmail.com>
2025-11-20 10:33:47 +0530

    added #757 solns

---
+++ PNo_757_Set_Intersection_Size_At_Least_Two.java
@@ -0,0 +1,76 @@
+/*
+You are given a 2D integer array intervals where intervals[i] = [starti, endi] represents all the integers from starti to endi inclusively.
+
+A containing set is an array nums where each interval from intervals has at least two integers in nums.
+
+For example, if intervals = [[1,3], [3,7], [8,9]], then [1,2,4,7,8,9] and [2,3,4,8,9] are containing sets.
+Return the minimum possible size of a containing set.
+
+
+
+Example 1:
+
+Input: intervals = [[1,3],[3,7],[8,9]]
+Output: 5
+Explanation: let nums = [2, 3, 4, 8, 9].
+It can be shown that there cannot be any containing array of size 4.
+Example 2:
+
+Input: intervals = [[1,3],[1,4],[2,5],[3,5]]
+Output: 3
+Explanation: let nums = [2, 3, 4].
+It can be shown that there cannot be any containing array of size 2.
+Example 3:
+
+Input: intervals = [[1,2],[2,3],[2,4],[4,5]]
+Output: 5
+Explanation: let nums = [1, 2, 3, 4, 5].
+It can be shown that there cannot be any containing array of size 4.
+
+
+Constraints:
+
+1 <= intervals.length <= 3000
+intervals[i].length == 2
+0 <= starti < endi <= 108
+*/
+
+class Solution {
+    public int intersectionSizeTwo(int[][] intervals) {
+        int idx = 0;
+        long[] packed = new long[intervals.length];
+
+        for (int[] seg : intervals) {
+            packed[idx] = -seg[0] & 0xFFFFFFFFL;
+            packed[idx++] |= (long) seg[1] << 32;
+        }
+
+        Arrays.sort(packed);
+
+        int low = -2;
+        int high = -1;
+        int left, right;
+        int count = 0;
+
+        for (long val : packed) {
+            left = -(int) val;
+            right = (int) (val >> 32);
+
+            if (left <= low) {
+                continue;
+            }
+
+            if (left <= high) {
+                count++;
+                low = high;
+            } else {
+                count += 2;
+                low = right - 1;
+            }
+
+            high = right;
+        }
+
+        return count;
+    }
+}
