b6e01533e42b4179391313de6e0be4cb14cde096
Nikhil Chikate <nikhilchikate07@gmail.com>
2025-12-28 09:11:35 +0530

    added #1351 solns

---
+++ PNo_1351_Count_Negative_Numbers_in_a_Sorted_Matrix.java
@@ -0,0 +1,47 @@
+/*
+Given a m x n matrix grid which is sorted in non-increasing order both row-wise and column-wise, return the number of negative numbers in grid.
+
+
+
+Example 1:
+
+Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+Output: 8
+Explanation: There are 8 negatives number in the matrix.
+Example 2:
+
+Input: grid = [[3,2],[1,0]]
+Output: 0
+
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 100
+-100 <= grid[i][j] <= 100
+
+
+Follow up: Could you find an O(n + m) solution?
+*/
+
+class Solution {
+    public int countNegatives(int[][] mat) {
+        final int rows = mat.length;
+        final int cols = mat[0].length;
+        int res = 0;
+        int r = rows - 1;
+        int c = 0;
+
+        while (r >= 0 && c < cols) {
+            if (mat[r][c] < 0) {
+                res += cols - c;
+                --r;
+            } else {
+                ++c;
+            }
+        }
+
+        return res;
+    }
+}
