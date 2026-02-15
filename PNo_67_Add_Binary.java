f2bd915b0b8d99484cc24408e71fdfe49fdcfa79
Nikhil Chikate <nikhilchikate07@gmail.com>
2026-02-15 11:12:16 +0530

    added #67 solns

--- PNo_67_Add_Binary.java
+++ PNo_67_Add_Binary.java
@@ -20,6 +20,7 @@ a and b consist only of '0' or '1' characters.
 Each string does not contain leading zeros except for the zero itself.
 */
 
+
 class Solution {
     public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
