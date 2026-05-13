37d2281a1dae95787e063eb0f041770070824be5
Nikhil Chikate <nikhilchikate07@gmail.com>
2026-05-13 08:46:29 +0530

    added 1674

---
+++ PNo_1674_Minimum_Moves_to_Make_Array_Complementary.java
@@ -0,0 +1,78 @@
+/*
+You are given an integer array nums of even length n and an integer limit. In one move, you can replace any integer from nums with another integer between 1 and limit, inclusive.
+
+The array nums is complementary if for all indices i (0-indexed), nums[i] + nums[n - 1 - i] equals the same number. For example, the array [1,2,3,4] is complementary because for all indices i, nums[i] + nums[n - 1 - i] = 5.
+
+Return the minimum number of moves required to make nums complementary.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,4,3], limit = 4
+Output: 1
+Explanation: In 1 move, you can change nums to [1,2,2,3] (underlined elements are changed).
+nums[0] + nums[3] = 1 + 3 = 4.
+nums[1] + nums[2] = 2 + 2 = 4.
+nums[2] + nums[1] = 2 + 2 = 4.
+nums[3] + nums[0] = 3 + 1 = 4.
+Therefore, nums[i] + nums[n-1-i] = 4 for every i, so nums is complementary.
+Example 2:
+
+Input: nums = [1,2,2,1], limit = 2
+Output: 2
+Explanation: In 2 moves, you can change nums to [2,2,2,2]. You cannot change any number to 3 since 3 > limit.
+Example 3:
+
+Input: nums = [1,2,1,2], limit = 2
+Output: 0
+Explanation: nums is already complementary.
+ 
+
+Constraints:
+
+n == nums.length
+2 <= n <= 105
+1 <= nums[i] <= limit <= 105
+n is even.
+*/
+
+class Solution {
+    public int minMoves(int[] nums, int limit) {
+        int n = nums.length;
+        int[] delta = new int[limit * 2 + 2];
+
+        int half = n / 2;
+        for (int i = 0; i < half; ++i) {
+            int a = nums[i];
+            int b = nums[n - 1 - i];
+
+            int min, max;
+            if (a < b) {
+                min = a;
+                max = b;
+            } else {
+                min = b;
+                max = a;
+            }
+
+            --delta[min + 1];
+            --delta[a + b];
+            ++delta[a + b + 1];
+            ++delta[max + limit + 1];
+        }
+
+        int ans = n;
+        int moves = n;
+
+        int maxTarget = limit * 2;
+        for (int i = 2; i <= maxTarget; ++i) {
+            moves += delta[i];
+            if (moves < ans) {
+                ans = moves;
+            }
+        }
+
+        return ans;
+    }
+}
