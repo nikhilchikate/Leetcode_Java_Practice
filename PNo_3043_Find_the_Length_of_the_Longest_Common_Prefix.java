f42b7edc04c6c16769185b5622e15bbfbd594de5
Nikhil Chikate <nikhilchikate07@gmail.com>
2026-05-21 06:28:00 +0530

    added 3043

---
+++ PNo_3043_Find_the_Length_of_the_Longest_Common_Prefix.java
@@ -0,0 +1,93 @@
+/*
+You are given two arrays with positive integers arr1 and arr2.
+
+A prefix of a positive integer is an integer formed by one or more of its digits, starting from its leftmost digit. For example, 123 is a prefix of the integer 12345, while 234 is not.
+
+A common prefix of two integers a and b is an integer c, such that c is a prefix of both a and b. For example, 5655359 and 56554 have common prefixes 565 and 5655 while 1223 and 43456 do not have a common prefix.
+
+You need to find the length of the longest common prefix between all pairs of integers (x, y) such that x belongs to arr1 and y belongs to arr2.
+
+Return the length of the longest common prefix among all pairs. If no common prefix exists among them, return 0.
+
+ 
+
+Example 1:
+
+Input: arr1 = [1,10,100], arr2 = [1000]
+Output: 3
+Explanation: There are 3 pairs (arr1[i], arr2[j]):
+- The longest common prefix of (1, 1000) is 1.
+- The longest common prefix of (10, 1000) is 10.
+- The longest common prefix of (100, 1000) is 100.
+The longest common prefix is 100 with a length of 3.
+Example 2:
+
+Input: arr1 = [1,2,3], arr2 = [4,4,4]
+Output: 0
+Explanation: There exists no common prefix for any pair (arr1[i], arr2[j]), hence we return 0.
+Note that common prefixes between elements of the same array do not count.
+ 
+
+Constraints:
+
+1 <= arr1.length, arr2.length <= 5 * 104
+1 <= arr1[i], arr2[i] <= 108 */
+
+class Solution {
+    private class TrieNode {
+        private final TrieNode[] children;
+
+        private TrieNode() {
+            children = new TrieNode[10];
+        }
+    }
+
+    private TrieNode root;
+    private final int[] arr = { 100000000, 10000000, 1000000, 100000, 10000, 1000, 100, 10, 1 };
+
+    private void add(int x) {
+        TrieNode node = root;
+        for (int i = count(x); i < 9; i++) {
+            if (node.children[x / arr[i]] == null) {
+                node.children[x / arr[i]] = new TrieNode();
+            }
+            node = node.children[x / arr[i]];
+            x %= arr[i];
+        }
+    }
+
+    private int find(int x) {
+        TrieNode node = root;
+        int ans = 0;
+        for (int i = count(x); i < 9; i++) {
+            if (node.children[x / arr[i]] == null) {
+                break;
+            }
+            node = node.children[x / arr[i]];
+            x %= arr[i];
+            ans++;
+        }
+        return ans;
+    }
+
+    private int count(int x) {
+        for (int i = 0; i < 9; i++) {
+            if (x >= arr[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int longestCommonPrefix(int[] arr1, int[] arr2) {
+        root = new TrieNode();
+        for (int i : arr1) {
+            add(i);
+        }
+        int ans = 0;
+        for (int i : arr2) {
+            ans = Math.max(ans, find(i));
+        }
+        return ans;
+    }
+}
