83090a8a40b47cb854316269d8a8c5f7a59ca490
Nikhil Chikate <nikhilchikate07@gmail.com>
2026-06-21 11:11:29 +0530

    added 1833

---
+++ PNo_1833_Maximum_Ice_Cream_Bars.java
@@ -0,0 +1,77 @@
+/*
+It is a sweltering summer day, and a boy wants to buy some ice cream bars.
+
+At the store, there are n ice cream bars. You are given an array costs of length n, where costs[i] is the price of the ith ice cream bar in coins. The boy initially has coins coins to spend, and he wants to buy as many ice cream bars as possible. 
+
+Note: The boy can buy the ice cream bars in any order.
+
+Return the maximum number of ice cream bars the boy can buy with coins coins.
+
+You must solve the problem by counting sort.
+
+ 
+
+Example 1:
+
+Input: costs = [1,3,2,4,1], coins = 7
+Output: 4
+Explanation: The boy can buy ice cream bars at indices 0,1,2,4 for a total price of 1 + 3 + 2 + 1 = 7.
+Example 2:
+
+Input: costs = [10,6,8,7,7,8], coins = 5
+Output: 0
+Explanation: The boy cannot afford any of the ice cream bars.
+Example 3:
+
+Input: costs = [1,6,3,1,2,5], coins = 20
+Output: 6
+Explanation: The boy can buy all the ice cream bars for a total price of 1 + 6 + 3 + 1 + 2 + 5 = 18.
+ 
+
+Constraints:
+
+costs.length == n
+1 <= n <= 105
+1 <= costs[i] <= 105
+1 <= coins <= 108
+*/
+
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        int n = costs.length;
+        int maxVal = costs[0];
+
+        for(int x: costs) {
+            maxVal = Math.max(maxVal,x);
+        }
+
+        int[] freq = new int[maxVal+1];
+
+        for(int x: costs) {
+            freq[x] += 1;
+        }
+
+        int totalCost = 0;
+        int totalBought = 0;
+        int i = 0;
+
+        for(; i < freq.length; i++) {
+            totalCost += freq[i] * i;
+            if(totalCost > coins) {
+                totalCost -= freq[i] * i;
+                int availableCost = coins - totalCost;
+                if(availableCost > 0 && i > 0) {
+                    int canTake = availableCost/i;
+                    if(canTake > 0) {
+                        return totalBought + canTake;
+                    }
+                }
+                return totalBought;
+            } else {
+                totalBought += freq[i];
+            }
+        }
+
+        return totalBought;
+    }
+}
