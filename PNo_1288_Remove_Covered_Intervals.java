45ed4aae3cc186699816b273095116dec5f70b20
Nikhil Chikate <nikhilchikate07@gmail.com>
2026-07-06 09:11:12 +0530

    added 1288

---
+++ PNo_1288_Remove_Covered_Intervals.java
@@ -0,0 +1,58 @@
+/*
+Given an array intervals where intervals[i] = [li, ri] represent the interval [li, ri), remove all intervals that are covered by another interval in the list.
+
+The interval [a, b) is covered by the interval [c, d) if and only if c <= a and b <= d.
+
+Return the number of remaining intervals.
+
+ 
+
+Example 1:
+
+Input: intervals = [[1,4],[3,6],[2,8]]
+Output: 2
+Explanation: Interval [3,6] is covered by [2,8], therefore it is removed.
+Example 2:
+
+Input: intervals = [[1,4],[2,3]]
+Output: 1
+ 
+
+Constraints:
+
+1 <= intervals.length <= 1000
+intervals[i].length == 2
+0 <= li < ri <= 105
+All the given intervals are unique.
+*/
+
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        int n = intervals.length;
+        int remaining = n;
+
+        for (int i = 0; i < n; i++) {
+            boolean covered = false;
+            int a = intervals[i][0];
+            int b = intervals[i][1];
+
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    continue;
+
+                int c = intervals[j][0];
+                int d = intervals[j][1];
+
+                if (c <= a && b <= d) {
+                    covered = true;
+                    break;
+                }
+            }
+            if (covered) {
+                remaining--;
+            }
+        }
+        
+        return remaining;
+    }
+}
