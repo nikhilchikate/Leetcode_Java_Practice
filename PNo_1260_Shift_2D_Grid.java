636de291a210132cdb6b899f76e6394145fc057b
Nikhil Chikate <nikhilchikate07@gmail.com>
2026-07-20 09:38:48 +0530

    added 1260 soln

---
+++ PNo_1260_Shift_2D_Grid.java
@@ -0,0 +1,67 @@
+/*
+Given a 2D grid of size m x n and an integer k. You need to shift the grid k times.
+
+In one shift operation:
+
+Element at grid[i][j] moves to grid[i][j + 1].
+Element at grid[i][n - 1] moves to grid[i + 1][0].
+Element at grid[m - 1][n - 1] moves to grid[0][0].
+Return the 2D grid after applying shift operation k times.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[1,2,3],[4,5,6],[7,8,9]], k = 1
+Output: [[9,1,2],[3,4,5],[6,7,8]]
+Example 2:
+
+
+Input: grid = [[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]], k = 4
+Output: [[12,0,21,13],[3,8,1,9],[19,7,2,5],[4,6,11,10]]
+Example 3:
+
+Input: grid = [[1,2,3],[4,5,6],[7,8,9]], k = 9
+Output: [[1,2,3],[4,5,6],[7,8,9]]
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m <= 50
+1 <= n <= 50
+-1000 <= grid[i][j] <= 1000
+0 <= k <= 100
+*/
+
+import java.util.AbstractList;
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        final int n = grid.length;
+        final int m = grid[0].length;
+        final int total = n * m;
+        final int shift = k % total;
+
+        return new AbstractList<List<Integer>>() {
+            public List<Integer> get(int i) {
+                return new AbstractList<Integer>() {
+                    public Integer get(int j) {
+                        int target1D = i * m + j;
+                        int source1D = (target1D + total - shift) % total;
+                        return grid[source1D / m][source1D % m];
+                    }
+
+                    public int size() {
+                        return m;
+                    }
+                };
+            }
+
+            public int size() {
+                return n;
+            }
+        };
+    }
+}
